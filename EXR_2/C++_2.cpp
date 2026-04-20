fa6135376aacb94c5b5d57d00037dbf30ff8e5d0
Danil <9139316075mok@gmail.com>
2026-04-20 10:43:18 +0700

    готов

--- EXR_2/C++_2.cpp
+++ EXR_2/C++_2.cpp
@@ -3,6 +3,8 @@
 #include <iomanip>
 #include <string>
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -19,11 +21,13 @@ long long mod_pow(long long base, long long exp, long long m) {
 }
 
 int main() {
+    srand(time(NULL));
+
     vector<bool> sieve(501, true);
     sieve[0] = sieve[1] = false;
-    for (int p = 2; p * p <= 500; p++) {
-        if (sieve[p]) {
-            for (int i = p * p; i <= 500; i += p) sieve[i] = false;
+    for (int p_s = 2; p_s * p_s <= 500; p_s++) {
+        if (sieve[p_s]) {
+            for (int i = p_s * p_s; i <= 500; i += p_s) sieve[i] = false;
         }
     }
 
@@ -31,72 +35,63 @@ int main() {
     cout << "Введите разрядность t: ";
     cin >> bits;
 
-
     int target_q_bits = (bits + 1) / 2; 
-    
     long long q_min = pow(2, target_q_bits - 1);
     long long q_max = pow(2, target_q_bits) - 1;
     
     unsigned int q = 0;
     string marker;
-
-    // Идем по решету, чтобы найти q из таблицы
     for (int i = q_min; i <= q_max && i <= 500; i++) {
         if (sieve[i]) {
             q = i;
-            cout << "Выбрано q = " << q <<  " Использовать это q? (+/-): ";
+            cout << "Выбрано q = " << q << ". Использовать? (+/-): ";
             cin >> marker;
-            if (marker == "+") break; 
+            if (marker == "+") break;
         }
     }
 
-    if (q == 0) {
-        cout << "Подходящее q не найдено!" << endl;
-        return 1;
-    }
+    if (q == 0) return 1;
 
-    unsigned int u = 0;
-    long long p = 0;
-    long long p_test = 1;
+    vector<long long> table_p;
 
-    while (p == 0) {
-        long long pow2 = pow(2, bits - 1); 
-        long long N = pow2 / q;
+    while (table_p.size() < 10) {
+        double xi = (double)rand() / RAND_MAX;
+        if (xi == 0) xi = 0.1;
+
+        long long N = floor(pow(2, bits - 1) / q) + floor((pow(2, bits - 1) * xi) / q);
+
         if (N % 2 != 0) N++;
 
-        p_test = (N + u) * q + 1;
-        
-        long long check1 = mod_pow(2, p_test - 1, p_test); 
-        long long check2 = mod_pow(2, N + u, p_test); 
+        unsigned int u = 0;
 
-        // Проверка попадания p в нужную разрядность t
-        if (p_test >= pow(2, bits - 1) && p_test < pow(2, bits)) {
+        while (true) {
+            long long p_candidate = (N + u) * q + 1;
+
+            if (p_candidate >= pow(2, bits)) break;
+
+            long long check1 = mod_pow(2, p_candidate - 1, p_candidate);
+            long long check2 = mod_pow(2, N + u, p_candidate);
+
             if (check1 == 1 && check2 != 1) {
-                p = p_test;
+                table_p.push_back(p_candidate);
                 break;
             }
-        }
 
-        u += 2; 
-        if (p_test > pow(2, bits)) {
-            cout << "В этом диапазоне p не найдено. Попробуйте другое q." << endl;
-            break;
+            u += 2;
         }
     }
 
     string border = "------------------------------------------------------------------------------------";
-    int w1 = 20, w2 = 8; 
+    int w1 = 20, w2 = 10; 
 
-    if (p != 0) {
-        cout << border << endl;
-        cout << "| " << left << setw(w1) << "№" << "|";
-        for (int i = 0; i < 10; i++) cout << setw(w2) << i + 1 << "|";
-        cout << endl << border << endl;
+    cout << border << endl;
+    cout << "| " << left << setw(w1) << "№" << "|";
+    for (int i = 0; i < 10; i++) cout << setw(w2) << i + 1 << "|";
+    cout << endl << border << endl;
 
-        cout << "| " << left << setw(w1) << "Простое число p" << "|";
-        for (int i = 0; i < 10; i++) cout << setw(w2) << p << "|";
-        cout << endl << border << endl;
-    }
+    cout << "| " << left << setw(w1) << "Простое число p" << "|";
+    for (int i = 0; i < 10; i++) cout << setw(w2) << table_p[i] << "|";
+    cout << endl << border << endl;
 
     return 0;
 }
